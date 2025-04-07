9a0b9dc9bc1e0691b182bc4e7e9ce307887050d5
Ángel de los Santos <angel.delossantos@idener.ai>
2025-04-07 12:33:55 +0200

    First commit

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # rewet-ghg-model
 
-Repo to host the python and R scripts for the ReWet GHG model defined in WP4
+Repo to host the Python and R scripts for the ReWet GHG model defined in WP4.
+
+Python version used: 3.11.9
+
+### Python
+
+First, create your own virtuan environment and activate it using the next commands
+- python -m venv .venv
+- .venv/Scripts/activate
+
+Secondly, install the libraries used with this command
+- pip install -r requirements.txt
---
+++ python-src/demo.py
@@ -0,0 +1,16 @@
+from scipy.optimize import differential_evolution
+
+import numpy as np
+import pandas as pd
+
+
+def ghg_model(x):
+    "Not even close"
+    arg1 = -0.2 * np.sqrt(0.5 * (x[0] ** 2 + x[1] ** 2))
+    arg2 = 0.5 * (np.cos(2. * np.pi * x[0]) + np.cos(2. * np.pi * x[1]))
+    return -20. * np.exp(arg1) - np.exp(arg2) + 20. + np.e
+
+bounds = [(-5, 5), (-5, 5)]
+result = differential_evolution(ghg_model, bounds, rng=1)
+print(result.x)
+print(result.fun)
